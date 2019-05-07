1e3a934adb59ae05598b9db848f2423c29010272
THOMPKINS <62592@GFPS.Local>
2019-05-07 10:42:07 -0600

    ...

---
+++ music/main.js
@@ -0,0 +1,46 @@
+/*
+ * Auto-generated content from the Brackets New Project extension.
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global $, window, document */
+
+// Simple jQuery event handler
+
+$(document).ready(function () {
+    var aNote = document.getElementById('aAudio');
+        $('#a').mousedown(function(){
+            aNote.currentTime = 0;
+            aNote.play();
+    });
+    var bNote = document.getElementById('bAudio');
+        $('#a').mousedown(function(){
+            bNote.currentTime = 0;
+            bNote.play();
+    });
+        var cNote = document.getElementById('cAudio');
+        $('#a').mousedown(function(){
+            cNote.currentTime = 0;
+            cNote.play();
+    });
+        var dNote = document.getElementById('dAudio');
+        $('#a').mousedown(function(){
+            dNote.currentTime = 0;
+            dNote.play();
+    });
+        var eNote = document.getElementById('eAudio');
+        $('#a').mousedown(function(){
+            eNote.currentTime = 0;
+            eNote.play();
+    });
+        var fNote = document.getElementById('fAudio');
+        $('#a').mousedown(function(){
+            fNote.currentTime = 0;
+            fNote.play();
+    });
+        var gNote = document.getElementById('gAudio');
+        $('#a').mousedown(function(){
+            gNote.currentTime = 0;
+            gNote.play();
+    });
+});
